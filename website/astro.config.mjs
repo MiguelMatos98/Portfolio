fdb725f952d3033fed3c48e208590039b4ffdba0
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2026-01-20 17:34:04 -0500

    feat: added a basic github portfolio.

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -6,26 +6,43 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
+    output: "static",
+  image: {
+    domains: ["images.unsplash.com"],
+  },
+  prefetch: true,
   integrations: [starlight({
-      title: 'My Docs',
+      title: 'MATOS',
       customCss: ['./src/styles/global.css'],
       social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
       sidebar: [
           {
               label: 'Guides',
-              items: [
-                  // Each item here is one entry in the navigation menu.
-                  { label: 'Example Guide', slug: 'guides/example' },
-              ],
+              autogenerate: { directory: 'guides' },
           },
           {
               label: 'Reference',
               autogenerate: { directory: 'reference' },
           },
+          {
+                label: 'Projects',
+                autogenerate: { directory: 'projects' },
+          },
+                    {
+                label: 'Work',
+                autogenerate: { directory: 'work' },
+          }
       ],
-  }), react()],
+
+      components: {
+            Header: './src/components/header/AstroStarlightHeader.astro',
+            Footer: './src/components/footer/AstroStarlightFooter.astro',
+      },
+  }), react(), mdx()],
 
   vite: {
     plugins: [tailwindcss()],
